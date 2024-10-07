d235550d7b4e2a3ff934314957d8632836379085
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-10-07 16:55:14 +0300

    mofifying

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove(); 
+}
+
+const newHeader = document.createElement('h1'); // Create the <h1> elmt
+
+newHeader.id = 'victory'; //set id as new header
+
+newHeader.innerHTML = 'Charles is the champion'
+
+
+
+
+
+
+
